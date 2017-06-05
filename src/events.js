9430cae54d4cc9ee2763f5b35816bfca2e7d9ff9
fiatjaf <fiatjaf@gmail.com>
2017-06-05 14:13:15 +0000

    track.any and select.any for getting custom messages from react.

--- src/events.js
+++ src/events.js
@@ -11,34 +11,41 @@ track.preventDefault = function (e) {
   track(e)
 }
 
-track.withValue = function (value) {
+track.withData = function (data) {
   return (e) => {
-    eventStreams['__global__'].shamefullySendNext({e, value, element: e.currentTarget})
+    eventStreams['__global__'].shamefullySendNext({e, data, element: e.currentTarget})
   }
 }
-track.preventDefault.withValue = function (value) {
+track.preventDefault.withData = function (data) {
   return (e) => {
     e.preventDefault()
-    eventStreams['__global__'].shamefullySendNext({e, value, element: e.currentTarget})
+    eventStreams['__global__'].shamefullySendNext({e, data, element: e.currentTarget})
   }
 }
 
-track.tag = function (name, value) {
+track.tag = function (name, data) {
   let eventStream = eventStreams[name] || xs.create()
   eventStreams[name] = eventStream
   return e => {
-    eventStreams['__global__'].shamefullySendNext({e, value, element: e.currentTarget})
-    eventStream.shamefullySendNext({e, value, element: e.currentTarget})
+    eventStreams['__global__'].shamefullySendNext({e, data, element: e.currentTarget})
+    eventStream.shamefullySendNext({e, data, element: e.currentTarget})
   }
 }
-track.preventDefault.tag = function (name, value) {
+track.preventDefault.tag = function (name, data) {
   let eventStream = eventStreams[name] || xs.create()
   eventStreams[name] = eventStream
   return e => {
     e.preventDefault()
-    eventStreams['__global__'].shamefullySendNext({e, value, element: e.currentTarget})
-    eventStream.shamefullySendNext({e, value, element: e.currentTarget})
+    eventStreams['__global__'].shamefullySendNext({e, data, element: e.currentTarget})
+    eventStream.shamefullySendNext({e, data, element: e.currentTarget})
   }
+}
+
+var anyStreams = xs.create()
+track.any = function (name, data) {
+  let anyStream = anyStreams[name] || xs.create()
+  anyStreams[name] = anyStream
+  anyStream.shamefullySendNext(data)
 }
 
 
@@ -52,12 +59,25 @@ export function select (selector, tag = '__global__') {
     .filter(meta => matchesSelector(meta.element, selector))
 
   ee.events = function (selectedType) {
-    return ee
+    ee = ee
       .filter(meta => meta.e.type === selectedType || !selectedType)
-      .map(meta => meta.e)
+
+    let normal = ee.map(meta => meta.e)
+
+    normal.withData = function () {
+      return ee.map(meta => ({event: meta.e, data: meta.data}))
+    }
+
+    return normal
   }
 
   return ee
+}
+
+select.any = function selectAny (name) {
+  let anyStream = anyStreams[name] || xs.create()
+  anyStreams[name] = anyStream
+  return anyStream
 }
 
 var matchesSelector
